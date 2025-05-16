e89b70b5c000f3015a51e3434c4253600d4d3ffe
Lucas Quaresma <lucks0110.quaresma@gmail.com>
2025-05-16 00:47:02 -0300

    Commit

--- app/settings.py
+++ app/settings.py
@@ -144,8 +144,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # pasta estática na raiz do projeto
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
